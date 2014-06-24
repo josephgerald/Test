2094770f581ae5ec7df66d551d66ca04b6145434
Joseph Gerald J <Gerald@Josephs-Mac.local>
2014-06-24 12:56:01 +0530

    Testing by adding files

---
+++ TestApp/TestApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by Joseph Gerald J on 6/24/14.
+//  Copyright (c) 2014 Trellisys.net Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestApp/TestApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestApp
+//
+//  Created by Joseph Gerald J on 6/24/14.
+//  Copyright (c) 2014 Trellisys.net Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
